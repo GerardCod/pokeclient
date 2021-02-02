302e0871cc9bad35c0a6c4e71d4e4f0545519f8a
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-02-02 14:47:03 -0600

    [MODIFY] Creating error interceptor.

--- src/app/services/interceptors/error.interceptor.ts
+++ src/app/services/interceptors/error.interceptor.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { catchError} from 'rxjs/operators';
+import { throwError } from 'rxjs';
 import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -12,7 +15,31 @@ export class ErrorInterceptor implements HttpInterceptor {
 
   constructor() {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        const status: number = error.status;
+        const message: string = error.message;
+
+        switch(status) {
+          case 400:
+            console.log(`Error de petición: ${message}`);
+            break;
+          case 403:
+            console.log(`No tienes permiso para este recurso: ${message}`);
+            break;
+          case 404:
+            console.log(`Ruta no encontrada: ${message}`);
+            break;
+          case 405:
+            console.log(`Método HTTP no permitido: ${message}`);
+            break;
+          case 500:
+            console.log(`Error del servidor: ${message}`);
+            break;
+        }
+        return throwError(error);
+      })
+    );
   }
 }
