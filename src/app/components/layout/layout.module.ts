3bada6c6b3a89b510b887876b312859b1e0ecaef
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-02-02 15:16:40 -0600

    [ADD] Dashboard component added.

--- src/app/components/layout/layout.module.ts
+++ src/app/components/layout/layout.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { SharedModule } from 'src/app/shared/components/shared/shared.module';
 import { LayoutComponent } from './layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
-  declarations: [LayoutComponent],
+  declarations: [LayoutComponent, DashboardComponent],
   imports: [
     CommonModule,
     LayoutRoutingModule,
