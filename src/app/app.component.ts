50713ac1f953f84a251970ed13a068fa96c4ea3e
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-02-03 11:26:57 -0600

    [MODIFY] Implementing Dashboard components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,24 +8,6 @@ import { PokemonService } from './services/pokemon.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent {
   title = 'dwp';
-  pokemonSubscription: Subscription;
-
-  constructor(private service: PokemonService) {}
-
-  ngOnInit(): void {
-    this.getPokemonByName('pikachu');
-  }
-  ngOnDestroy(): void {
-    this.pokemonSubscription.unsubscribe();
-  }
-
-  getPokemonByName(name: string): void {
-    this.pokemonSubscription = this.service.getPokemonByName(name).subscribe(
-      (data: Pokemon) => {
-        console.log(data);
-      }
-    );
-  }
 }
--- src/app/components/layout/dashboard/dashboard.component.ts
+++ src/app/components/layout/dashboard/dashboard.component.ts
@@ -1,15 +1,35 @@
+import { OnDestroy } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Pokemon } from 'src/app/models/pokemon';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
+  pokemonSubscription: Subscription;
+  pokemon: Pokemon;
 
-  constructor() { }
-
+  constructor(private service: PokemonService) { }
+  
   ngOnInit(): void {
+    this.getPokemon('pikachu');
+  }
+
+  ngOnDestroy(): void {
+    this.pokemonSubscription.unsubscribe();
+  }
+
+  getPokemon(name: string): void {
+    this.pokemonSubscription = this.service.getPokemonByName(name)
+    .subscribe(
+      (data: Pokemon) => {
+        this.pokemon = data;
+      }
+    )
   }
 
 }
