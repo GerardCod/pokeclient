10e1bc4c957393fd3f6ceb882d54218c26bd13a0
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-02-03 11:34:12 -0600

    [MODIFY] Error interceptor modified.

--- src/app/services/interceptors/error.interceptor.ts
+++ src/app/services/interceptors/error.interceptor.ts
@@ -23,19 +23,19 @@ export class ErrorInterceptor implements HttpInterceptor {
 
         switch(status) {
           case 400:
-            console.log(`Error de petición: ${message}`);
+            console.error(`Error de petición: ${message}`);
             break;
           case 403:
-            console.log(`No tienes permiso para este recurso: ${message}`);
+            console.error(`No tienes permiso para este recurso: ${message}`);
             break;
           case 404:
-            console.log(`Ruta no encontrada: ${message}`);
+            console.error(`Ruta no encontrada: ${message}`);
             break;
           case 405:
-            console.log(`Método HTTP no permitido: ${message}`);
+            console.error(`Método HTTP no permitido: ${message}`);
             break;
           case 500:
-            console.log(`Error del servidor: ${message}`);
+            console.error(`Error del servidor: ${message}`);
             break;
         }
         return throwError(error);
