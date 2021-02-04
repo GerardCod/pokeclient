139386891bb4aaca63f570b92f9a04803e07ac92
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-02-04 08:28:20 -0600

    [ADD] Movements component added

--- src/app/components/layout/layout.module.ts
+++ src/app/components/layout/layout.module.ts
@@ -9,10 +9,11 @@ import { PokemonCardComponent } from './dashboard/pokemon-card/pokemon-card.comp
 import { PokemonTypeComponent } from './dashboard/pokemon-type/pokemon-type.component';
 import { AbilitiesComponent } from './dashboard/abilities/abilities.component';
 import { ItemsComponent } from './dashboard/items/items.component';
+import { MovementsComponent } from './dashboard/movements/movements.component';
 
 
 @NgModule({
-  declarations: [LayoutComponent, DashboardComponent, PokemonCardComponent, PokemonTypeComponent, AbilitiesComponent, ItemsComponent],
+  declarations: [LayoutComponent, DashboardComponent, PokemonCardComponent, PokemonTypeComponent, AbilitiesComponent, ItemsComponent, MovementsComponent],
   imports: [
     CommonModule,
     LayoutRoutingModule,
