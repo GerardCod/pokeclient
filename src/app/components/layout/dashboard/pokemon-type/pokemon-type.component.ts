dd4be705be70838528ea03e74c6c16e1d6269177
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-02-04 08:10:05 -0600

    [ADD] Pokemon type component added-

--- src/app/components/layout/dashboard/pokemon-type/pokemon-type.component.ts
+++ src/app/components/layout/dashboard/pokemon-type/pokemon-type.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { Type } from 'src/app/models/pokemon';
 @Component({
   selector: 'app-pokemon-type',
   templateUrl: './pokemon-type.component.html',
   styleUrls: ['./pokemon-type.component.scss']
 })
 export class PokemonTypeComponent implements OnInit {
+  @Input() type: Type;
 
   constructor() { }
 
