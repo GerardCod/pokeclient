75b2ddea75c49098ec545ab95191ec023069d21c
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-02-04 09:08:56 -0600

    [MODIFY] Pokemon model modfified.

--- src/app/models/pokemon.ts
+++ src/app/models/pokemon.ts
@@ -7,36 +7,28 @@ export class Pokemon {
         public weight: number,
         public sprites: Sprites,
         public types: Type[],
-        public abilities: Ability[]
+        public abilities: Ability[],
+        public moves: Move[],
+        public items: Item[]
     ) {}
+}
+
+export class Move {
+    constructor(public move: Definition, public version_group_details: LevelLearned[]) {}
 }
 
 export class Type {
     constructor(
         public slot: number,
-        public type: TypeDefinition
+        public type: Definition
     ){}
-}
-
-export class TypeDefinition {
-    constructor(
-        public name: string,
-        public url: string
-    ) {}
 }
 
 export class Ability {
     constructor(
         public slot: number,
         public is_hidden: boolean,
-        public ability: AbilityDefinition
-    ) {}
-}
-
-export class AbilityDefinition {
-    constructor(
-        public name: string,
-        public url: string
+        public ability: Definition
     ) {}
 }
 
@@ -45,4 +37,20 @@ export class Sprites {
         public back_default: string,
         public front_default: string
     ) {}
+}
+
+export class Definition {
+    constructor(public name: string, public url: string) {}
+}
+
+export class LevelLearned {
+    constructor(public level_learned_at: number, public move_learn_method: Definition, public version_group: Definition) {}
+}
+
+export class Item {
+    constructor(public item: Definition, public version_details: VersionDetail[]) {}
+}
+
+export class VersionDetail {
+    constructor(public rarity: number, public version: Definition) {}
 }
